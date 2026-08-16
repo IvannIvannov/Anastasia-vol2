3631f8e3c0ddd8f3f842f13b407a6def9c09d04f
IvannIvannov <151462104+IvannIvannov@users.noreply.github.com>
2026-08-16 14:44:44 +0300

    Setup Tailwind CSS and Motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <main>
-      <h1>Anastasia Paskaleva</h1>
+    <main className="min-h-screen bg-[#f3efe8] flex items-center justify-center">
+      <h1 className="text-6xl font-medium tracking-tight">
+        Anastasia Paskaleva
+      </h1>
     </main>
   );
 }
